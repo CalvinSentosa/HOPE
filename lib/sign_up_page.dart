549c46e2f237cba0242a2e89f195ee9823eb8b7b
Eneper <3nver.2510@gmail.com>
2024-12-09 22:01:50 +0700

    Test Push

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -32,6 +32,7 @@ class SignUpPage extends StatelessWidget {
       }
     }
 
+    // Halooo Cuy
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -42,38 +43,41 @@ class SignUpPage extends StatelessWidget {
             children: [
               // Header Image
               Positioned(
-              top: 0, // Mengatur jarak gambar dari atas
-              left: 0, // Mengatur jarak gambar dari kiri
-              right: 0, // Mengatur gambar agar sejajar dengan kanan
-              child: Align(
-                alignment: Alignment.topCenter, // Posisi gambar di tengah atas
-                child: Stack(
-                  clipBehavior: Clip.none, // Agar gambar tidak terpotong oleh container
-                  children: [
-                    // Setengah lingkaran sebagai hiasan
-                    ClipPath(
-                      clipper: OvalClipper(),
-                      child: Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF9BB168), // Green background
+                top: 0, // Mengatur jarak gambar dari atas
+                left: 0, // Mengatur jarak gambar dari kiri
+                right: 0, // Mengatur gambar agar sejajar dengan kanan
+                child: Align(
+                  alignment:
+                      Alignment.topCenter, // Posisi gambar di tengah atas
+                  child: Stack(
+                    clipBehavior:
+                        Clip.none, // Agar gambar tidak terpotong oleh container
+                    children: [
+                      // Setengah lingkaran sebagai hiasan
+                      ClipPath(
+                        clipper: OvalClipper(),
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF9BB168), // Green background
+                          ),
                         ),
                       ),
-                    ),
-                    // Gambar logo
-                    Positioned(
-                      top: 0, // Gambar lebih ke atas agar tampak di atas setengah lingkaran
-                      left: 50,
-                      child: Image.asset(
-                        'Assets/logo_gambar.png',
-                        width: 250,
-                        height: 250,
+                      // Gambar logo
+                      Positioned(
+                        top:
+                            0, // Gambar lebih ke atas agar tampak di atas setengah lingkaran
+                        left: 50,
+                        child: Image.asset(
+                          'Assets/logo_gambar.png',
+                          width: 250,
+                          height: 250,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
               Center(
                 child: Column(
                   children: [
@@ -81,7 +85,8 @@ class SignUpPage extends StatelessWidget {
                       height: 120,
                       decoration: BoxDecoration(
                         image: DecorationImage(
-                          image: AssetImage("assets/images/header.png"), // Ganti dengan gambar header Anda
+                          image: AssetImage(
+                              "assets/images/header.png"), // Ganti dengan gambar header Anda
                           fit: BoxFit.cover,
                         ),
                       ),
@@ -99,25 +104,26 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 32),
-              
+
               // Email Field
               TextField(
                 decoration: InputDecoration(
                   labelText: "Email Address",
                   hintText: "Enter your email...",
                   prefixIcon: Icon(Icons.email),
-                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 1),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    // Mengatur warna border ketika fokus
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4), // Hijau ketika fokus
-                      borderRadius: BorderRadius.circular(30),
-                    ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  // Mengatur warna border ketika fokus
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.green, width: 4), // Hijau ketika fokus
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
               ),
               SizedBox(height: 16),
@@ -129,7 +135,8 @@ class SignUpPage extends StatelessWidget {
                   labelText: "Password",
                   hintText: "Enter your password...",
                   prefixIcon: Icon(Icons.lock),
-                  suffixIcon: Icon(Icons.visibility_off), // Tambahkan logika toggle jika diperlukan
+                  suffixIcon: Icon(Icons
+                      .visibility_off), // Tambahkan logika toggle jika diperlukan
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
@@ -139,7 +146,8 @@ class SignUpPage extends StatelessWidget {
                   ),
                   // Mengatur warna border ketika fokus
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 4), // Hijau ketika fokus
+                    borderSide: BorderSide(
+                        color: Colors.green, width: 4), // Hijau ketika fokus
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
@@ -153,7 +161,8 @@ class SignUpPage extends StatelessWidget {
                   labelText: "Password Confirmation",
                   hintText: "Confirm your password...",
                   prefixIcon: Icon(Icons.lock),
-                  suffixIcon: Icon(Icons.visibility_off), // Tambahkan logika toggle jika diperlukan
+                  suffixIcon: Icon(Icons
+                      .visibility_off), // Tambahkan logika toggle jika diperlukan
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
@@ -163,7 +172,8 @@ class SignUpPage extends StatelessWidget {
                   ),
                   // Mengatur warna border ketika fokus
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 4), // Hijau ketika fokus
+                    borderSide: BorderSide(
+                        color: Colors.green, width: 4), // Hijau ketika fokus
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
@@ -205,7 +215,7 @@ class SignUpPage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
+                    );
                   },
                   child: Text(
                     "Already have an account? Sign In.",
