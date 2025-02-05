4c1f64acd8476965509e295a1de2cf1e28670da3
CalvinSentosa <calvinsentosa21@gmail.com>
2025-02-05 10:19:38 +0700

    Insert Chatbot Page

--- lib/Chatbot/chatbot.dart
+++ lib/Chatbot/chatbot.dart
@@ -1,5 +1,8 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
+
+import 'consts.dart';
 
 class ChatPage extends StatefulWidget{
   const ChatPage({super.key});
@@ -9,10 +12,21 @@ class ChatPage extends StatefulWidget{
 
 class _ChatPageState extends State<ChatPage>{
 
+  final _openAI = OpenAI.instance.build(
+    token: OPENAI_API_KEY, 
+    baseOption: HttpSetup(
+      receiveTimeout: const Duration(
+        seconds: 5,
+      ),
+    ),
+    enableLog: true,
+  );
+
   final ChatUser _currentUser = ChatUser(id:'1', firstName: 'Calvin', lastName: 'Sentosa');
   final ChatUser _gptChatUser = ChatUser(id:'2', firstName: 'Chat', lastName: 'GPT');
 
   List<ChatMessage> _messages = <ChatMessage>[];
+  List<ChatUser> _typingUsers = <ChatUser>[];
 
   @override
   Widget build (BuildContext context){
@@ -28,6 +42,12 @@ class _ChatPageState extends State<ChatPage>{
       ),
       body: DashChat(
         currentUser: _currentUser, 
+        typingUsers: _typingUsers,
+        messageOptions: const MessageOptions(
+          currentUserContainerColor: Colors.black,
+          containerColor: Color.fromRGBO(0, 166,126, 1),
+          textColor: Colors.white,
+        ),
         onSend: (ChatMessage m){
           getChatResponse(m);
         }, 
@@ -35,6 +55,28 @@ class _ChatPageState extends State<ChatPage>{
     );
   }
   Future<void> getChatResponse(ChatMessage m)async{
-    
+    setState((){
+      _messages.insert(0, m);
+      _typingUsers.add(_gptChatUser);
+    });
+    List <Messages> _messagesHistory = _messages.reversed.map((m){
+      if (m.user == _currentUser){
+        return Messages(role:Role.user, content:m.text);
+      }else{
+        return Messages(role: Role.assistant, content: m.text);
+      }
+    }).toList();
+    final request = ChatCompleteText(model: Gpt4ChatModel(), messages: _messagesHistory, maxToken: 200,);
+    final response = await _openAI.onChatCompletion(request: request);
+    for (var element in response!.choices){
+      if (element.message != null){
+        setState(() {
+          _messages.insert(0, ChatMessage(user: _gptChatUser, createdAt: DateTime.now(), text: element.message!.content));
+        });
+      }
+    }
+    setState(() {
+      _typingUsers.remove(_gptChatUser);
+    });
   }
 }
--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:project_android_studio/Home/appbar.dart';
+
+import '../Chatbot/chatbot.dart';
 // import 'package:project_android_studio/Test/resPage.dart';
 // import 'package:flutter_tester/test_heart/test_heart.dart';
 
@@ -18,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
   List<Widget> body = [
     const AppBarApp(),
-    const Icon(CupertinoIcons.home),
+    const ChatPage(),
     // const MyHomePage()
     const Icon(CupertinoIcons.square_list)
     // DepressionResultPage(weeklyScores: [100, 75, 50, 25, 50, 75, 100])
