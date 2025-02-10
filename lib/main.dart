a7df333ca834024d43dcb29607b6ef3f99df0494
Eneper <3nver.2510@gmail.com>
2025-02-10 14:08:05 +0700

    Update Homepage and resolve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_android_studio/Home/home_page.dart';
-<<<<<<< HEAD
 // import 'Register/landing_pages.dart';
-=======
-import 'Register/landing_pages.dart';
->>>>>>> c5153ba5624ffa05bb14416329be7f803aeefc6d
 
 void main() {
   runApp(MyApp());
@@ -15,21 +11,10 @@ final GlobalKey<HomePageState> homePageKey = GlobalKey();
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("Helo");
-    print(DefaultTextStyle.of(context).style.fontFamily);
-<<<<<<< HEAD
-    print("Helo");
-    print(DefaultTextStyle.of(context).style.fontFamily);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: HomePage(key: homePageKey),
-=======
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      home: LandingPage1(),
->>>>>>> c5153ba5624ffa05bb14416329be7f803aeefc6d
     );
   }
 }
